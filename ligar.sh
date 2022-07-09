b7bd09388f8ca606a54add46a1b79dec179cd9ad
joao.lucas <joao.lucas@14mob.com>
2022-07-08 23:20:20 -0300

    ajuste

--- ligar.sh
+++ ligar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nohup java BikeLock > log.txt 2> errors.txt < /dev/null &
+nohup java -cp .:mssql-jdbc-10.2.1.jre11.jar BikeLock > log.txt 2> errors.txt < /dev/null &
 PID=$!
 echo $PID > pid.txt
 
